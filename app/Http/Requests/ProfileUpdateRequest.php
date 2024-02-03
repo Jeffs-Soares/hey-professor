a73762430040df628e23ada9b37eda7f701b4954
Jeff-Soares <soares.jeffersonthadeu@gmail.com>
2024-02-03 09:16:42 -0300

    pint update 2

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -4,8 +4,7 @@ namespace App\Http\Requests;
 
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
-use Illuminate\Validation\Rule as ContractRule;
+use Illuminate\Validation\{Rule, Rule as ContractRule};
 
 class ProfileUpdateRequest extends FormRequest
 {
@@ -17,7 +16,7 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name'  => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
